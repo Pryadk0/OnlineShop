46b4120546013840d39d29c2ac87dffa52c4d7f8
Pryadko Sergey <kewlsoerge@gmail.com>
2023-03-23 15:37:01 +0300

    Fragment Page1: LiveData usage for remote downloadable products added.

--- app/src/main/java/com/example/testapplication/presentation/viewmodels/AuthorizedViewModel.kt
+++ app/src/main/java/com/example/testapplication/presentation/viewmodels/AuthorizedViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.testapplication.presentation.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.testapplication.domain.FlashSaleProduct
 import com.example.testapplication.domain.GetAllProductsUseCase
@@ -12,8 +14,19 @@ class AuthorizedViewModel @Inject constructor(
     private val getSearchWordsUseCase: GetSearchWordsUseCase
 ) : ViewModel() {
 
-    fun getAllProducts(callback: (List<LatestProduct>, List<FlashSaleProduct>) -> Unit) {
-        getAllProductsUseCase.getAllProducts(callback)
+    private val _latestProductsLiveData: MutableLiveData<List<LatestProduct>> = MutableLiveData()
+    val latestProductsLiveData: LiveData<List<LatestProduct>>
+        get() = _latestProductsLiveData
+    private val _flashSaleProductLiveData: MutableLiveData<List<FlashSaleProduct>> =
+        MutableLiveData()
+    val flashSaleProductLiveData: LiveData<List<FlashSaleProduct>>
+        get() = _flashSaleProductLiveData
+
+    fun updateAllProductsLiveData() {
+        getAllProductsUseCase.getAllProducts { latestProductsList, flashSaleProductsList ->
+            _latestProductsLiveData.value = latestProductsList
+            _flashSaleProductLiveData.value = flashSaleProductsList
+        }
     }
 
     fun getSearchWords(callback: (List<String>) -> Unit) {
